6a91bd3e00a98b3e0bcae75b2bf10b7d3099d9ff
Leonel Tchassou <leonel.tchassou@proton.me>
2025-07-06 23:15:23 +0100

    nvim: Removed the line talking about folding in the lsp-requirements

--- .config/nvim/lua/config/lsp-requirements.lua
+++ .config/nvim/lua/config/lsp-requirements.lua
@@ -51,10 +51,10 @@ end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
 
-M.capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true,
-}
+-- M.capabilities.textDocument.foldingRange = {
+--     dynamicRegistration = false,
+--     lineFoldingOnly = true,
+-- }
 
 M.capabilities.textDocument.completion.completionItem = {
     documentationFormat = { "markdown", "plaintext" },
